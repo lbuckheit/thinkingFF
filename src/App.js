301608cec727d03c32976f079b053a657fd9f811
Luke Buckheit <lukebuckheit@gmail.com>
2019-08-10 19:20:49 -0400

    more abstraction done and seems to be working correctly

--- src/App.js
+++ src/App.js
@@ -19,10 +19,11 @@ let QBArr = []
 let RBArr = []
 let WRArr = []
 let TEArr = []
-let positionArr = [QBArr, RBArr, WRArr, TEArr]
+let unsortedPositionArr = [QBArr, RBArr, WRArr, TEArr]
+let sortedPositionArr = []
 
 for (let i = 0; i < inputs.length; i++) {
-  let playerArr = positionArr[i]
+  let playerArr = unsortedPositionArr[i]
   let playerData = inputs[i].default
 
   //Pushing the single player objects to an array
@@ -35,11 +36,34 @@ for (let i = 0; i < inputs.length; i++) {
 
   //Sort by whatever you want (in this case, I am sorting by ADP on the x axis, and then the y axis represents the results of your personal algorithm)
   playerArr.sort((a, b) => a.ADP - b.ADP)
+
+  //Push a sorted version of the array to sortedPositionArr, which will be used inside the component
+  sortedPositionArr.push([...playerArr])
 }
 
 //This function calculates the results of your personal algorigthm (for use inside the component lifecycle methods)
-function algoScore(wr, weightsObj = {}) {
-  let AScore = Number(wr.receiving_yds * weightsObj['receiving_yds'] || 0) + Number(wr.receiving_tds * weightsObj['receiving_tds'] || 0) + Number(wr.receiving_rec * weightsObj['receiving_rec'] || 0) + Number(wr.receiving_tar * weightsObj['receiving_tar'] || 0)
+function algoScore(player, weightsObj = {}, selectedPositionIndex) {
+  let AScore
+  switch (selectedPositionIndex) {
+    case 0:
+      let qb = player
+      console.log('IN QB')
+      break
+    case 1:
+      let rb = player
+      console.log('IN RB')
+      break
+    case 2:
+      let wr = player
+      AScore = Number(wr.receiving_yds * weightsObj['receiving_yds'] || 0) + Number(wr.receiving_tds * weightsObj['receiving_tds'] || 0) + Number(wr.receiving_rec * weightsObj['receiving_rec'] || 0) + Number(wr.receiving_tar * weightsObj['receiving_tar'] || 0)
+      break
+    case 3:
+      let te = player
+      console.log('IN TE')
+      break
+    default:
+      return
+  }
   return AScore > 0 ? AScore : 0
 }
 
@@ -58,11 +82,15 @@ class App extends React.Component {
     //weightsObj: a dictionary mapping the name of the statistic to its weight in the algorithm
     //data: an array of single player objects with the current algorithm score as a property on each player
     //gamesPlayed: a bool that determines whether to scale the data by games played in 2018
+    //playerData: an array of arrays of singleplayers that is generated as the first thing in this file
+    //selectedPositionIndex: a number that indicated which element of state.playerData to operate on
     this.state = {
       algoComponents: [],
       weightsObj: {},
       graphingData: [],
-      gamesPlayed: false
+      gamesPlayed: false,
+      playerData: [...sortedPositionArr],
+      selectedPositionIndex: 2
     }
   }
 
@@ -76,18 +104,19 @@ class App extends React.Component {
     })
   }
 
-  updateData(weightsObj) {
+  updateData(weightsObj, positionIndex) {
     //Create a new array, push a copy of each player object to it, set the algorithm score as a property on each player, and set the updated graphing data on state
     let newArr = []
-    for (let elem of WRArr) {
+    console.log(positionIndex)
+    for (let elem of this.state.playerData[positionIndex]) {
       newArr.push({...elem})
     }
     for (let i = 0; i < newArr.length; i++) {
       //Determining whether to scale by games played
       if (!this.state.gamesPlayed) {
-        newArr[i].AScore = algoScore(newArr[i], weightsObj)
+        newArr[i].AScore = algoScore(newArr[i], weightsObj, positionIndex)
       } else {
-        newArr[i].AScore = algoScore(newArr[i], weightsObj) / newArr[i].games
+        newArr[i].AScore = algoScore(newArr[i], weightsObj, positionIndex) / newArr[i].games
       }
 
       //This position property is necessary for selecting the elements to show tooltips in the chart component
@@ -96,11 +125,15 @@ class App extends React.Component {
     this.setState({ graphingData: [...newArr]})
   }
 
+  componentDidMount() {
+    console.log(this.state.playerData)
+  }
+
   handleGamesPlayed() {
     //Toggling the gamesPlayed bool on and off
     this.setState({gamesPlayed: !this.state.gamesPlayed}, function() {
       //Updating the data as soon as you toggle and the new state is set
-      this.updateData(this.state.weightsObj, !this.state.gamesPlayed)
+      this.updateData(this.state.weightsObj, this.state.selectedPositionIndex)
     })
   }
 
@@ -109,7 +142,7 @@ class App extends React.Component {
     let newWeights = {...this.state.weightsObj}
     newWeights[statistic] = value
     this.setState({ weightsObj: newWeights})
-    this.updateData(newWeights)
+    this.updateData(newWeights, this.state.selectedPositionIndex)
   }
 
   render() {
